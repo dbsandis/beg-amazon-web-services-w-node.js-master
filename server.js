67f2b848fd9ca4a8717d7f4e9bfaeec2eaf1b5a0
dbsandis <dbsandis@gmail.com>
2017-05-21 22:46:10 -0400

    Added FS & Multer as require

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var debug = require('debug')('photoalbums');
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,7 @@ var globals = require('./lib/globals');
 var mysql       = require('mysql');
 var app = express();
 
+var upload = multer({ dest: './tmp/' })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,6 +24,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
